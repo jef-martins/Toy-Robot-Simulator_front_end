1765572ebfb784b87595f5fa9b636d69d048856f
Jefferson Martins <jef.martins.s@gmail.com>
2022-04-02 20:47:08 -0300

    first commit

--- Server/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./service");
-const rotas = require("../Routes/rotas");
-
-app.listen(8081, () => {
-    console.log("Server On in port 8081 at", new Date().toLocaleString());
-});
